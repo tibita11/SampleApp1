bdba66d59eb8380afaba63774fc4ee338df820fc
tibita11 <tibita0811@gmail.com>
2023-10-15 17:47:26 +0900

    initial commit

---
+++ SampleApp1/MainTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarController.swift
+//  SampleApp1
+//
+//  Created by 鈴木楓香 on 2023/10/15.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .systemBackground
+    }
+}
